80aaf8cc6b6c6c95f06bb5269bf6ac850f049a0f
andreassenmarvin <marvinandreassen@gmail.com>
2021-10-14 22:57:07 +0300

    Initial commit

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ const squares = Array.from(document.querySelectorAll('.grid div'))
 const alienInvaders = [
   0,1,2,3,4,5,6,7,8,9,
   15,16,17,18,19,20,21,22,23,24,
-  30,31,32,33,34,35,36,37,38,39
+  30,31,32,33,34,35,36,37,38,39,
+  45,46,47,48,49,50,51,52,53,54
 ]
 
 function draw() {
