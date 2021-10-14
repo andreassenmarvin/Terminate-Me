48ef8e9f95109b34f9ca064d860bf7240976e25a
andreassenmarvin <marvinandreassen@gmail.com>
2021-10-14 22:42:17 +0300

    Add shoot function

--- app.js
+++ app.js
@@ -97,3 +97,36 @@ const alienInvaders = [
       }
     }
     invadersId = setInterval(moveInvaders, 600)
+
+    function shoot(e) {
+        let laserId
+        let currentLaserIndex = currentShooterIndex
+        function moveLaser() {
+          squares[currentLaserIndex].classList.remove('laser')
+          currentLaserIndex -= width
+          squares[currentLaserIndex].classList.add('laser')
+      
+          if (squares[currentLaserIndex].classList.contains('invader')) {
+            squares[currentLaserIndex].classList.remove('laser')
+            squares[currentLaserIndex].classList.remove('invader')
+            squares[currentLaserIndex].classList.add('boom')
+      
+            setTimeout(()=> squares[currentLaserIndex].classList.remove('boom'), 300)
+            clearInterval(laserId)
+      
+            const alienRemoved = alienInvaders.indexOf(currentLaserIndex)
+            aliensRemoved.push(alienRemoved)
+            results++
+            resultsDisplay.innerHTML = results
+            console.log(aliensRemoved)
+      
+          }
+      
+        }
+        switch(e.key) {
+          case 'Space':
+            laserId = setInterval(moveLaser, 100)
+        }
+      }
+      
+      document.addEventListener('keydown', shoot)
