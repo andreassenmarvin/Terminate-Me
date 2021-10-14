309282b9cbe2186b9ef9bfc0cb14455c484b9a5b
andreassenmarvin <marvinandreassen@gmail.com>
2021-10-14 22:38:42 +0300

    Add keys for moving shooter

--- app.js
+++ app.js
@@ -38,4 +38,18 @@ const alienInvaders = [
   }
   
   squares[currentShooterIndex].classList.add('shooter')
+
+  function moveShooter(e) {
+    squares[currentShooterIndex].classList.remove('shooter')
+    switch(e.key) {
+      case 'ArrowLeft':
+        if (currentShooterIndex % width !== 0) currentShooterIndex -=1
+        break
+      case 'ArrowRight' :
+        if (currentShooterIndex % width < width -1) currentShooterIndex +=1
+        break
+    }
+    squares[currentShooterIndex].classList.add('shooter')
+  }
+  document.addEventListener('keydown', moveShooter)
   
