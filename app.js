a3db04b373d2ce8bed06b681949b24d748d18189
andreassenmarvin <marvinandreassen@gmail.com>
2021-10-14 22:36:47 +0300

    Add invaders

---
+++ app.js
@@ -0,0 +1,32 @@
+const grid = document.querySelector('.grid')
+const resultsDisplay = document.querySelector('.results')
+let currentShooterIndex = 202
+let width = 15
+let direction = 1
+let invadersId
+let goingRight = true
+let aliensRemoved = []
+let results = 0
+
+for (let i = 0; i < 225; i++) {
+    const square = document.createElement('div')
+    grid.appendChild(square)
+}
+
+const squares = Array.from(document.querySelectorAll('.grid div'))
+
+const alienInvaders = [
+    0,1,2,3,4,5,6,7,8,9,
+    15,16,17,18,19,20,21,22,23,24,
+    30,31,32,33,34,35,36,37,38,39
+  ]
+
+  function draw() {
+    for (let i = 0; i < alienInvaders.length; i++) {
+      if(!aliensRemoved.includes(i)) {
+        squares[alienInvaders[i]].classList.add('invader')
+      }
+    }
+  }
+
+  draw()
